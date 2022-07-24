553a079d0559953c3a03789abfb0222f5698ff82
Emilio Pino <pinoen@yahoo.com.ar>
2022-07-24 16:57:00 -0300

    se agrega validacion de formulario de contacto en contacto.js

--- js/contacto.js
+++ js/contacto.js
@@ -1,1 +1,32 @@
-console.log("hable con nosotros.");
+let nombre = document.querySelector("#nombre");
+let errorNombre = document.querySelector("#errorNombre");
+
+let mensaje = document.querySelector("#mensaje");
+let errorMensaje = document.querySelector("#errorMensaje");
+
+let enviado = document.querySelector("#enviado");
+
+function validar() {
+  if (nombre.value === "") {
+    errorNombre.innerHTML = "Este campo debe completarse!";
+  } else if (nombre.value.length > 40) {
+    errorNombre.innerHTML = "El nombre debe contener maximo 40 caracteres.";
+  } else if (mensaje.value === "") {
+    errorMensaje.innerHTML = "Este campo debe completarse!";
+  } else if (mensaje.value > 120) {
+    errorMensaje.innerHTML = "El mensaje debe contener maximo 120 caracteres.";
+  } else {
+    enviado.innerHTML = `Muchas gracias ${nombre.value}. Tu mensaje fue enviado correctamente. Te contactaremos pronto.`;
+    errorNombre.innerHTML = "";
+    errorMensaje.innerHTML = "";
+    nombre.value = "";
+    mensaje.value = "";
+  }
+}
+
+let boton = document.querySelector(".boton__contacto");
+
+boton.addEventListener("click", (e) => {
+  e.preventDefault();
+  validar();
+});

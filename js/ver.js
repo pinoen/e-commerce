ffd9d96ef698a295169417cdf9c6a8b0cc89eb48
Emilio Pino <pinoen@yahoo.com.ar>
2022-07-25 23:17:15 -0300

    se agrega vinculo desde home page para ver todos los productos disponibles en determinada categoria a traves de ver.html, ver.css y ver.js

---
+++ js/ver.js
@@ -0,0 +1,43 @@
+let container__starWars = document.querySelector(".imagenes__starWars");
+let container__consolas = document.querySelector(".imagenes__consolas");
+let container__diversos = document.querySelector(".imagenes__diversos");
+
+let buscar = document.querySelector(".input__header");
+
+let agregarProducto = async (palabra) => {
+  let uri = "http://localhost:3000/productos?_sort=precio&_order=desc";
+  if (palabra) {
+    uri += `&q=${palabra}`;
+  }
+
+  const respuesta = await fetch(uri);
+  const productos = await respuesta.json();
+
+  productos.forEach((producto) => {
+    template = `
+      <div class="box__${producto.categoria}">
+        <img src="${producto.imagen}" alt="" class="foto" />
+        <p class="description">${producto.nombre}</p>
+        <p class="precio">$ ${producto.precio}</p>
+        <p class="ver">#000${producto.id}</p>
+      </div>
+      `;
+    if (producto.categoria === "starWars") {
+      container__starWars.innerHTML += template;
+    }
+    if (producto.categoria === "consolas") {
+      container__consolas.innerHTML += template;
+    }
+    if (producto.categoria === "diversos") {
+      container__diversos.innerHTML += template;
+    }
+  });
+};
+
+buscar.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    agregarProducto(buscar.value);
+  }
+});
+
+window.addEventListener("DOMContentLoaded", () => agregarProducto());

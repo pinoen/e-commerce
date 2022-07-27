c129b260ad874b88c9ad8412d97f59d1c800aa36
Emilio Pino <pinoen@yahoo.com.ar>
2022-07-27 12:45:46 -0300

    se agrega funcionalidad a boton editar para la edicion de productos en productos.js

--- js/productos.js
+++ js/productos.js
@@ -21,6 +21,7 @@ let agregarProducto = async (palabra) => {
       <p class="description">${producto.nombre}</p>
       <p class="precio">$ ${producto.precio}</p>
       <p class="ver">#000${producto.id}</p>
+      <div class="box__edicion" id="box__edicion${producto.id}"></div>
     </div>
     `;
   });
@@ -29,13 +30,69 @@ let agregarProducto = async (palabra) => {
   let eliminar = document.querySelectorAll(".eliminar");
   eliminar.forEach((btn) => {
     btn.addEventListener("click", async (e) => {
-      let boxPadre = e.target.parentNode;
-      let id = boxPadre.lastElementChild.innerHTML.slice(4);
+      let boxPadre = e.target.parentNode.children;
+      let id = boxPadre[5].innerHTML.slice(4);
 
       const res = await fetch("http://localhost:3000/productos/" + id, {
         method: "delete",
       });
       boxPadre.remove();
+    });
+  });
+
+  let editar = document.querySelectorAll(".editar");
+  editar.forEach((btn) => {
+    btn.addEventListener("click", async (e) => {
+      let boxPadre = e.target.parentNode.children;
+      let datosProductos = {
+        imagen: boxPadre[0].src.slice(22),
+        nombre: boxPadre[3].innerHTML,
+        precio: boxPadre[4].innerHTML.slice(2),
+        id: boxPadre[5].innerHTML.slice(4),
+      };
+
+      let boxEditar = document.querySelector(
+        `#box__edicion${datosProductos.id}`
+      );
+
+      boxEditar.style.display = "flex";
+      boxEditar.innerHTML = `
+        <form class="datos-producto" action="" method="post">
+          <h3>Editar producto</h3>
+          <input type="text" name="url" id="url" value="${datosProductos.imagen}">
+          <input type="text" name="categoria" id="categoria" value="Categoria">
+          <input type="text" name="nombre" id="nombre" value="${datosProductos.nombre}">
+          <input type="number" step=".01" name="precio" id="precio" value="${datosProductos.precio}">
+          <textarea name="descripcion" id="descripcion" placeholder="Descripcion del producto"></textarea>
+          <button type="submit" class="guardar">Guardar cambios</button>
+          <span id="agregado"></span>
+        </form>
+      `;
+
+      let formulario = document.querySelector(".datos-producto");
+      let btn = document.querySelector(".guardar");
+      btn.addEventListener("click", async (e) => {
+        const doc = {
+          imagen: formulario.url.value,
+          categoria: formulario.categoria.value,
+          nombre: formulario.nombre.value,
+          precio: formulario.precio.value,
+          descripcion: formulario.descripcion.value,
+          id: datosProductos.id,
+        };
+        console.log(formulario);
+        const response = await fetch(
+          "http://localhost:3000/productos/" + datosProductos.id,
+          {
+            method: "put",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(doc),
+          }
+        );
+        boxEditar.style.display = "none";
+      });
     });
   });
 };

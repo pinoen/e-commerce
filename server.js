06e2a7e0859372a8747369e4bdb7c8a3444ec966
Emilio Pino <pinoen@yahoo.com.ar>
2022-07-28 16:46:16 -0300

    se cambia ubicacion de productos.json

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("public/data/productos.json");
+const router = jsonServer.router("productos.json");
 
 const middlewares = jsonServer.defaults();
 

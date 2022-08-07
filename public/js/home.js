4d8a0971404b7960cea4151f671772f2a3abef2d
Emilio Pino <pinoen@yahoo.com.ar>
2022-08-07 12:12:29 -0300

    se agrega funcionalidad a barra de buscar productos

--- public/js/home.js
+++ public/js/home.js
@@ -1,14 +1,14 @@
 let container__starWars = document.querySelector(".imagenes__starWars");
 let container__consolas = document.querySelector(".imagenes__consolas");
 let container__diversos = document.querySelector(".imagenes__diversos");
+let busqueda = document.querySelector(".busqueda");
+let container__busqueda = document.querySelector(".imagenes__busqueda");
+let nuevaBusqueda = document.querySelector(".boton__busqueda");
 
 let buscar = document.querySelector(".input__header");
 
-let agregarProducto = async (palabra) => {
+let agregarProducto = async () => {
   let uri = "https://e-commerce-api-pinoen.herokuapp.com/productos";
-  if (palabra) {
-    uri += `?&q=${palabra}`;
-  }
 
   const respuesta = await fetch(uri);
   const productos = await respuesta.json();
@@ -34,10 +34,36 @@ let agregarProducto = async (palabra) => {
   });
 };
 
+let buscarProductos = async (palabra) => {
+  let uri = "https://e-commerce-api-pinoen.herokuapp.com/productos";
+  if (palabra) {
+    uri += `?&q=${palabra}`;
+  }
+
+  const respuesta = await fetch(uri);
+  const productos = await respuesta.json();
+
+  productos.forEach((producto) => {
+    template = `
+      <div class="box__${producto.categoria}">
+        <img src="${producto.imagen}" alt="" class="foto" />
+        <p class="description">${producto.nombre}</p>
+        <p class="precio">$ ${producto.precio}</p>
+        <p class="ver">#000${producto.id}</p>
+      </div>
+      `;
+  });
+  container__busqueda.innerHTML += template;
+};
+
 buscar.addEventListener("keydown", (e) => {
   if (e.key === "Enter") {
-    agregarProducto(buscar.value);
+    buscarProductos(buscar.value);
+    busqueda.style.display = "flex";
   }
+  nuevaBusqueda.addEventListener("click", () => {
+    window.location.reload();
+  });
 });
 
 window.addEventListener("DOMContentLoaded", () => agregarProducto());
